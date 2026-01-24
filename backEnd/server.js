003ba894ce72d774316ef781c059cd43846080a5
manishsuthar01 <manishsuthar78775@gmail.com>
2026-01-24 16:10:00 +0530

    implemented login for static file

--- backEnd/server.js
+++ backEnd/server.js
@@ -3,11 +3,13 @@ dotenv.config();
 
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const indexRouter = require("./routes/index");
 const connectToMongoDb = require("./db/index");
 const helmet = require("helmet");
 const morgan = require("morgan");
+
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -18,7 +20,14 @@ app.use(morgan("combined"));
 app.use(cookieParser());
 app.use(express.json()); //to parse the incoming req with json payload
 
+// used to serve static files such as html ,css images etc from the frontend
+app.use(express.static(path.join(__dirname, "../front_end/dist")));
+
 // ROUTES
+
+app.use((req,res)=>{
+  return res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+});
 app.use("/api", indexRouter);
 
 app.listen(PORT, () => {
